b31cef5d91e59f702710390d5995ffe8a9d38bdf
Utyf <utyff@mail.ru>
2018-03-26 12:55:40 +0300

    init sequence merge

--- usr/src/lcd.c
+++ usr/src/lcd.c
@@ -234,54 +234,71 @@ void LCD_Init(void) {
 }
 
 void LCD_Init_sequence() {
-    LCD_WR_REG(0xCF);
+    LCD_WR_REG(0xCF);  //  Power Control B
     LCD_WR_DATA(0x00);
     LCD_WR_DATA(0xC1);
-    LCD_WR_DATA(0X30);
-    LCD_WR_REG(0xED);
+    LCD_WR_DATA(0x30);
+
+    LCD_WR_REG(0xED);  //  Power on Sequence control
     LCD_WR_DATA(0x64);
     LCD_WR_DATA(0x03);
-    LCD_WR_DATA(0X12);
-    LCD_WR_DATA(0X81);
-    LCD_WR_REG(0xE8);
+    LCD_WR_DATA(0x12);
+    LCD_WR_DATA(0x81);
+
+    LCD_WR_REG(0xE8);  //  Driver timing control A
     LCD_WR_DATA(0x85);
-    LCD_WR_DATA(0x10);
-    LCD_WR_DATA(0x7A);
-    LCD_WR_REG(0xCB);
+    LCD_WR_DATA(0x10); // 00 - x10
+    LCD_WR_DATA(0x7A); // 78 - x7A
+
+    LCD_WR_REG(0xCB);   //  Power Control A
     LCD_WR_DATA(0x39);
     LCD_WR_DATA(0x2C);
     LCD_WR_DATA(0x00);
     LCD_WR_DATA(0x34);
     LCD_WR_DATA(0x02);
-    LCD_WR_REG(0xF7);
+
+    LCD_WR_REG(0xF7);   //  Pump ratio control
     LCD_WR_DATA(0x20);
-    LCD_WR_REG(0xEA);
+
+    LCD_WR_REG(0xEA);   //  Driver timing control B
     LCD_WR_DATA(0x00);
     LCD_WR_DATA(0x00);
-    LCD_WR_REG(0xC0);    //Power control
-    LCD_WR_DATA(0x1B);   //VRH[5:0]
-    LCD_WR_REG(0xC1);    //Power control
-    LCD_WR_DATA(0x01);   //SAP[2:0];BT[3:0]
-    LCD_WR_REG(0xC5);    //VCM control
-    LCD_WR_DATA(0x30);     //3F
-    LCD_WR_DATA(0x30);     //3C
-    LCD_WR_REG(0xC7);    //VCM control2
-    LCD_WR_DATA(0XB7);
-    LCD_WR_REG(0x36);    // Memory Access Control
+
+    LCD_WR_REG(0xC0);   //  Power Control 1
+    LCD_WR_DATA(0x1B); // VRH[5:0]  10 - 0x1B     !!!!!!! 3.65 V - 4.20 V
+
+    LCD_WR_REG(0xC1);   //  Power Control 2
+    LCD_WR_DATA(0x01); // SAP[2:0];BT[3:0]  10 - 01  !!!!!!!
+
+    LCD_WR_REG(0xC5);   //  VCOM Control 1
+    LCD_WR_DATA(0x30); // 3E - 30  /3F
+    LCD_WR_DATA(0x30); // 28 - 30  /3C
+
+    LCD_WR_REG(0xC7);   //  VCOM Control 2
+    LCD_WR_DATA(0xB7); //  86 - B7 (86 - плохое качество)
+
+    LCD_WR_REG(0x36);   // Memory Access Control
     LCD_WR_DATA(0x48);
-    LCD_WR_REG(0x3A);
-    LCD_WR_DATA(0x55);
+
+    LCD_WR_REG(0x3A);   //  Pixel Format Set
+    LCD_WR_DATA(0x55); // 16bit
+
     LCD_WR_REG(0xB1);
     LCD_WR_DATA(0x00);
-    LCD_WR_DATA(0x1A);
-    LCD_WR_REG(0xB6);    // Display Function Control
-    LCD_WR_DATA(0x0A);
-    LCD_WR_DATA(0xA2);
-    LCD_WR_REG(0xF2);    // 3Gamma Function Disable
-    LCD_WR_DATA(0x00);
-    LCD_WR_REG(0x26);    //Gamma curve selected
-    LCD_WR_DATA(0x01);
-    LCD_WR_REG(0xE0);    //Set Gamma
+    LCD_WR_DATA(0x1A); // Частота кадров 79 Гц // 18 - 1A  79Hz - 73Hz
+
+    LCD_WR_REG(0xB6);   // Display Function Control
+    LCD_WR_DATA(0x0A); // 08 - 0A
+    LCD_WR_DATA(0xA2); // 82 - A2
+    //LCD_WR_DATA8(0x27); //320 строк // отсутсвует
+
+    LCD_WR_REG(0xF2);    //  3Gamma Function
+    LCD_WR_DATA(0x00);  // Disable
+
+    LCD_WR_REG(0x26);    // Gamma curve selected
+    LCD_WR_DATA(0x01);  // Gamma Curve (G2.2) (Кривая цветовой гаммы)
+
+    LCD_WR_REG(0xE0);    // Positive Gamma  Correction
     LCD_WR_DATA(0x0F);
     LCD_WR_DATA(0x2A);
     LCD_WR_DATA(0x28);
@@ -297,7 +314,8 @@ void LCD_Init_sequence() {
     LCD_WR_DATA(0x00);
     LCD_WR_DATA(0x00);
     LCD_WR_DATA(0x00);
-    LCD_WR_REG(0XE1);    //Set Gamma
+
+    LCD_WR_REG(0xE1);     // Negative Gamma  Correction
     LCD_WR_DATA(0x00);
     LCD_WR_DATA(0x15);
     LCD_WR_DATA(0x17);
@@ -313,6 +331,7 @@ void LCD_Init_sequence() {
     LCD_WR_DATA(0x3F);
     LCD_WR_DATA(0x3F);
     LCD_WR_DATA(0x0F);
+
     LCD_WR_REG(0x2B);
     LCD_WR_DATA(0x00);
     LCD_WR_DATA(0x00);
@@ -323,9 +342,10 @@ void LCD_Init_sequence() {
     LCD_WR_DATA(0x00);
     LCD_WR_DATA(0x00);
     LCD_WR_DATA(0xef);
-    LCD_WR_REG(0x11); //Exit Sleep
+
+    LCD_WR_REG(0x11); // Exit Sleep
     delay_ms(120);
-    LCD_WR_REG(0x29); //display on
+    LCD_WR_REG(0x29); // display on
 }
 
 // Clear screen function
